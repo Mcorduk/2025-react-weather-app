2b253702e22f1a10ae84d79c1b4632af685a93f4
Mcorduk <om.corduk@gmail.com>
2025-02-28 19:00:31 +0300

    feat: add map icon and style the icon positionings

--- src/components/TabBar.tsx
+++ src/components/TabBar.tsx
@@ -1,17 +1,21 @@
 // export interface ITabBarProps {}
 import { Menu } from "lucide-react";
 import { Search } from "lucide-react";
+import { MapPinned } from "lucide-react";
 
 export default function TabBar() {
   return (
-    <div className=" flex justify-between absolute w-full h-[100px] bottom-0 left-0 items-center">
+    <div className=" absolute w-full h-[100px] bottom-0 left-0 flex items-center">
       <img
         className="rounded-3xl z-3 absolute"
         src="/src/assets/images/TabBar.png"
         alt=""
       />
-      <Search color="white" size={48} className="z-4" />
-      <Menu color="white" size={48} className="z-4" />
+      <div className="z-4 flex justify-between items-end w-full p-7">
+        <MapPinned color="white" size={48} className="z-4" />
+        <Search color="white" size={48} className="z-4 mb-3" />
+        <Menu color="white" size={48} className="z-4" />
+      </div>
     </div>
   );
 }
