6be83fd726a47f2a4990818da9b436c96c2f8feb
Mcorduk <om.corduk@gmail.com>
2025-03-09 12:47:19 +0300

    refactor: vertical forecast for better readability

--- src/components/VerticalForecast.tsx
+++ src/components/VerticalForecast.tsx
@@ -11,24 +11,23 @@ export default function VerticalForecast({
   forecastType,
 }: IVerticalForecastProps) {
   const { timeLabel, condition, temperature, chanceOfPrecipitation } = forecast;
+  const isCurrentTime = ["Now", "Today"].includes(timeLabel);
 
-  const currentTime =
-    timeLabel === "Now" || timeLabel === "Today" ? true : false;
   return (
     <div
       className={`content min-w-[60px] min-h-[140px] flex flex-col justify-around border border-gray-500 rounded-4xl py-2 shadow ${
-        currentTime ? "bg-solid-blue" : "inactive"
+        isCurrentTime ? "bg-solid-blue" : "inactive"
       }`}
     >
       <p>{formatTimeLabel(timeLabel, forecastType)}</p>
       <div>
-        {" "}
-        <img src={`/src/assets/images/icons/small/${condition}.png`} alt="" />
-        <p className="text-sm font-normal">
-          {chanceOfPrecipitation === 0
-            ? ""
-            : chanceOfPrecipitation?.toString().concat("%")}
-        </p>
+        <img
+          src={`/src/assets/images/icons/small/${condition}.png`}
+          alt={condition}
+        />
+        {chanceOfPrecipitation > 0 && (
+          <p className="text-sm font-normal">{chanceOfPrecipitation}%</p>
+        )}
       </div>
       <p>{temperature}°C</p>
     </div>
